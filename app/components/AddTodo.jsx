0134d653bf48b640673ef22b574407f856ec27f4
Dustin <dustin.sappier@learningspurs.com>
2017-03-24 14:31:05 -0300

    create addTodo component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  onFormSubmit: function (e){
+    e.preventDefault();
+
+    var todoText = this.refs.todoText.value;
+
+    if(todoText.length > 0){
+      this.refs.todoText.value = '';
+      this.props.onAddTodo(todoText);
+    }else{
+      this.refs.todoText.focus();
+    }
+  },
+  render: function () {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input type="text" ref="todoText" placeholder="Add to todo list.."/>
+          <button className="button expanded hollow">Add</button>
+        </form>
+      </div>
+    )
+  }
+});
+
+module.exports = AddTodo;
---
+++ app/tests/components/AddTodo.spec.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jquery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+  it('should exist', () => {
+    expect(AddTodo).toExist();
+  });
+
+  it('should call onAddTodo prop with valid data', () => {
+      var todoText = 'do something';
+      var spy = expect.createSpy();
+      var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+      var $el = $(ReactDOM.findDOMNode(addTodo));
+
+      addTodo.refs.todoText.value = todoText;
+      TestUtils.Simulate.submit($el.find('form')[0]);
+
+      expect(spy).toHaveBeenCalledWith(todoText);
+  });
+  it('should not call onAddTodo prop with invalid data', () => {
+      var todoText = '';
+      var spy = expect.createSpy();
+      var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+      var $el = $(ReactDOM.findDOMNode(addTodo));
+
+      addTodo.refs.todoText.value = todoText;
+      TestUtils.Simulate.submit($el.find('form')[0]);
+
+      expect(spy).toNotHaveBeenCalled();
+  });
+});
