84b20776520aa541cd72490de96d0ee0970f08c5
Dustin <dustin.sappier@bluespurs.com>
2017-03-27 14:00:24 -0300

    add TodoApi - unit tests

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,37 +1,21 @@
 var React = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-var uuid = require('node-uuid')
+var TodoApi = require('TodoApi');
 
 var TodoApp = React.createClass({
     getInitialState: function () {
       return {
         showCompleted: false,
         searchText: '',
-        todos: [
-          {
-            id: uuid(),
-            text: 'stuff',
-            completed: false
-          },
-          {
-            id: uuid(),
-            text: 'things',
-            completed: true
-          },
-          {
-            id: uuid(),
-            text: 'more stuff',
-            completed: false
-          },
-          {
-            id: uuid(),
-            text: 'more things',
-            completed: false
-          }
-        ]
+        todos: TodoApi.getTodos()
       };
+    },
+    componentDidUpdate: function () {
+      TodoApi.setTodos(this.state.todos);
     },
     handleAddTodo: function (text) {
       this.setState({
