4c24b9fb691966fc763f49a3243388fcab2e2462
Dustin <dustin.sappier@bluespurs.com>
2017-03-28 09:45:21 -0300

    edited TodoApp tests

--- app/tests/components/TodoApp.spec.jsx
+++ app/tests/components/TodoApp.spec.jsx
@@ -21,13 +21,16 @@ describe('TodoApp', () => {
     todoApp.handleAddTodo(todoText);
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
   it('should toggle completed value when handleToggle called', () => {
     var todoData = {
       id: 123,
       text: 'Unit tests',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
     var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
     todoApp.setState({
@@ -37,5 +40,24 @@ describe('TodoApp', () => {
     expect(todoApp.state.todos[0].completed).toBe(false);
     todoApp.handleToggle(123);
     expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+  });
+  it('should toggle todo completed to incomplete', () => {
+    var todoData = {
+      id: 123,
+      text: 'Unit tests',
+      completed: true,
+      createdAt: 0,
+      completedAt: 2
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({
+      todos: [todoData]
+    });
+
+    expect(todoApp.state.todos[0].completed).toBe(true);
+    todoApp.handleToggle(123);
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
   });
 });
