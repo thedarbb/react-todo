7b606e369240ee6ee751279e50a2db8ae14b358f
Dustin <dustin.sappier@bluespurs.com>
2017-03-29 13:18:16 -0300

    add showCompleted and searchText reducers

---
+++ app/tests/reducers/reducers.spec.jsx
@@ -0,0 +1,28 @@
+var expect = require('expect');
+var reducers = require('reducers');
+var df = require('deep-freeze-strict');
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set searchText', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        text: 'some text'
+      };
+      var response = reducers.searchTextReducer(df(''), df(action));
+
+      expect(response).toEqual(action.searchText);
+    });
+  });
+
+  describe('showCompletedReducer', () => {
+    it('should toggle showCompleted', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+      var response = reducers.showCompletedReducer(df(false), df(action));
+
+      expect(response).toEqual(true);
+    });
+  });
+});
