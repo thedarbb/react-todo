3ef3fee4841edc86cc175baff12ef469df282064
Dustin <dustin.sappier@bluespurs.com>
2017-04-03 14:52:53 -0300

    add actions for firebase login/logout

--- app/app.jsx
+++ app/app.jsx
@@ -3,20 +3,12 @@ var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-var TodoApp = require('TodoApp');
+import Login from 'Login';
+import TodoApp from 'TodoApp';
 
 var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoApi = require('TodoApi');
-
-// store.subscribe(() => {
-//   var state = store.getState();
-//   console.log('New state ', state);
-//   TodoApi.setTodos(state.todos);
-// });
-
-// var initialTodos = TodoApi.getTodos();
-// store.dispatch(actions.addTodos(initialTodos));
 
 store.dispatch(actions.startAddTodos());
 
@@ -28,7 +20,12 @@ require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
     <Provider store={store}>
-      <TodoApp/>
+      <Router history={hashHistory}>
+        <Route path="/">
+          <IndexRoute component={Login}/>
+          <Route path="/todos" component={TodoApp}/>
+        </Route>
+      </Router>
     </Provider>,
     document.getElementById('app')
 );
--- app/firebase/index.js
+++ app/firebase/index.js
@@ -14,5 +14,6 @@ try {
   console.log(e);
 }
 
+export var githubProvider = new firebase.auth.GithubAuthProvider();
 export var firebaseRef = firebase.database().ref();
 export default firebase;
