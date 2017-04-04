966e2f7583b3438c28c5b17958b96d7f526e5a27
Dustin <dustin.sappier@bluespurs.com>
2017-04-04 10:40:24 -0300

    update todos location to include uid

--- app/app.jsx
+++ app/app.jsx
@@ -12,14 +12,13 @@ import router from 'app/router/';
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     store.dispatch(actions.login(user.uid));
+    store.dispatch(actions.startAddTodos());
     hashHistory.push('/todos');
   } else {
     store.dispatch(actions.logout());
     hashHistory.push('/');
   }
 });
-
-store.dispatch(actions.startAddTodos());
 
 // Load foundation
 $(document).foundation();
